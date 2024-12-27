502bd1824aaba33f33b31ffcdfc61692fc74b0f1
Kishore <kishoreviji826@gmail.com>
2024-12-27 17:08:25 +0530

    initial commit

---
+++ src/main/java/com/example/library_APIGateway/LibraryApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.example.library_APIGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class LibraryApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApiGatewayApplication.class, args);
+		System.out.println("-----------------APIGateway Started-------------");
+	}
+
+}
---
+++ src/test/java/com/example/library_APIGateway/LibraryApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.library_APIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
